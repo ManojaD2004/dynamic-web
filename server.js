5f2467db8e595030aa99abb4ea02fff71c279ac8
Manoja D 🍏 <manojadkc2004@gmail.com>
2025-03-11 19:10:36 +0000

    name update

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
 const express = require("express");
 const app = express();
+const fs = require("fs");
 
-app.get("/", (req, res) => {
+app.get("/api", (req, res) => {
     try {
         const domain = req.get('host');
         console.log(domain);
         res.status(200).send({ message: `You are from subdomain ${domain}` });
+    } catch (error) {
+        console.log(error);
+        res.status(400).send({
+            status: "error", error: error
+        });
+    }
+});
+
+app.get("/", (req, res) => {
+    try {
+        const domain = req.get('host');
+        const userName = domain.split(".")[0];
+        let htmlTemplate = fs.readFileSync("./template.html", { encoding: "utf8" });
+        htmlTemplate = htmlTemplate.replace("{{{UserName}}}", userName);
+        console.log(domain);
+        res.status(200).send(htmlTemplate);
     } catch (error) {
         console.log(error);
         res.status(400).send({
