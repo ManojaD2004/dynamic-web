2eab22f392db5bd03ea29718b9efe5de5303c54f
Manoja D 🍏 <manojadkc2004@gmail.com>
2025-03-11 18:42:28 +0000

    test

--- server.js
+++ server.js
@@ -3,13 +3,17 @@ const app = express();
 
 app.get("/", (req, res) => {
     try {
-
+        const domain = req.get('host');
+        console.log(domain);
+        res.status(200).send({ message: `You are from subdomain ${domain}` });
     } catch (error) {
         console.log(error);
-        res.status(400).send({ status: "error", error: error })
+        res.status(400).send({
+            status: "error", error: error
+        });
     }
 });
 
 app.listen(3000, () => {
-    console.log("Server is listening!");
+    console.log("Server is listening to port 3000!");
 });
